14b65dd5c63ed70e423aafdf0d4eb75b4674a81b
David Mwaifunga <mwaifungad@gmail.com>
2022-09-08 10:38:07 +0200

    Corrected duplicate check query

--- expenseTracker/src/main/kotlin/com/assignment/expenseTracker/repositories/CategoryRepository.kt
+++ expenseTracker/src/main/kotlin/com/assignment/expenseTracker/repositories/CategoryRepository.kt
@@ -13,6 +13,6 @@ Category Repository.
 interface CategoryRepository : JpaRepository<Category, Int>{
 
     //Checking if there is a category with the same name in the DB.
-    @Query("select c from category c where lower(c.categoryName) like lower(concat('%', ?1,'%'))")
+    @Query("select c from category c where lower(c.categoryName) like lower(concat(?1))")
     fun findByNameFree(categoryName:String): List<Category?>
 }
